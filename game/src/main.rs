beec2369ee85e8379a3cdc1b199bf80831384e37
FlyWolfe <mjsherry248@gmail.com>
2024-07-27 22:54:24 -0400

    Initial Setup

--- game/src/main.rs
+++ game/src/main.rs
@@ -3,6 +3,7 @@ use bevy::{
     window::{WindowMode, WindowResolution},
 };
 use game_lib::GamePlugin;
+use space_bevy_xpbd_plugin::XpbdPlugin;
 use space_prefab::prelude::{PrefabBundle, PrefabPlugin};
 
 fn main() {
@@ -14,12 +15,12 @@ fn main() {
             title: "Your Game".into(),
             resolution: WindowResolution::new(1600., 900.),
             visible: true,
-            mode: WindowMode::Fullscreen,
+            mode: WindowMode::Windowed,
             ..default()
         }),
         ..default()
     }))
-    .add_plugins((PrefabPlugin, GamePlugin))
+    .add_plugins((PrefabPlugin, GamePlugin, XpbdPlugin))
     .add_systems(Startup, setup)
     .run();
 }
@@ -27,19 +28,9 @@ fn main() {
 fn setup(mut commands: Commands, _assets: Res<AssetServer>) {
     // prefab loaded by adding PrefabLoader component to any entity (it will be parent of prefab) or with prefab bundle
     commands
-        .spawn(PrefabBundle::new("scenes/cube.scn.ron"))
+        .spawn(PrefabBundle::new("scenes/play_scene.scn.ron"))
         .insert(Name::new("Prefab"));
 
-    // light
-    commands.spawn(PointLightBundle {
-        point_light: PointLight {
-            intensity: 1500.0,
-            shadows_enabled: true,
-            ..default()
-        },
-        transform: Transform::from_xyz(4.0, 8.0, 4.0),
-        ..default()
-    });
     // camera
     commands.spawn(Camera3dBundle {
         transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
